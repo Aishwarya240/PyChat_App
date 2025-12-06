f2b151a7881d29e6d981a1a6a26e9663487e6f30
Aishwarya <aishwaryapatil845@gmail.com>
2025-12-06 21:14:19 +0530

    Update main
    updated main.py. Added continuous question streaming and exit option.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from ollama import Client
 
-client = Client()
+isContinue = True
 
-messages = [
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-]
+while isContinue:
+    question = input("What's your question? Type 'exit' to quit.\n")
+    if question == "exit":
+        print("Thank you for playing!")
+        isContinue = False
+        isContinue = False
+    else:
+        client = Client()
 
-for part in client.chat('llama2', messages=messages, stream=True):
-  print(part.message.content, end='', flush=True)
+        messages = [
+          {
+            'role': 'user',
+            'content': question,
+          },
+        ]
+
+        for part in client.chat('llama2', messages=messages, stream=True):
+          print(part.message.content, end='', flush=True)
+        print("\n")

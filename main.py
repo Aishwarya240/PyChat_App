5089eab3fc8e1e170dec42738b9932b78424a044
Aishwarya <aishwaryapatil845@gmail.com>
2025-12-06 21:07:28 +0530

    updated main
    added llama2 model

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello 5001 World")
+from ollama import Client
+
+client = Client()
+
+messages = [
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+]
+
+for part in client.chat('llama2', messages=messages, stream=True):
+  print(part.message.content, end='', flush=True)

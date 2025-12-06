699d2d6c3b240276ace1e5a897318a80df6cd4aa
Aishwarya <aishwaryapatil845@gmail.com>
2025-12-06 21:23:37 +0530

    Added terminal color

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ollama import Client
+from colorama import Fore, Back, Style
 
 isContinue = True
 
 while isContinue:
-    question = input("What's your question? Type 'exit' to quit.\n")
+    question = input(Fore.RED + "What's your question? Type 'exit' to quit.\n")
     if question == "exit":
-        print("Thank you for playing!")
+        print(Fore.BLUE + "Thank you for playing!")
         isContinue = False
         isContinue = False
     else:
@@ -19,5 +20,5 @@ while isContinue:
         ]
 
         for part in client.chat('llama2', messages=messages, stream=True):
-          print(part.message.content, end='', flush=True)
+          print(Fore.GREEN + part.message.content, end='', flush=True)
         print("\n")
